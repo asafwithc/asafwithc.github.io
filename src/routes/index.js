88e6408a6a04005c149173179e6f09e8e016e701
asaf <asafemin06@gmail.com>
2023-08-28 09:51:17 +0300

    .

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
+    next();
 });
 
 module.exports = router;
--- src/routes/login.js
+++ src/routes/login.js
@@ -4,26 +4,26 @@ var axios = require('axios');
 var router = express.Router();
 
 router.post('/', async (req, res, next) => {
-    let token = req.body.token;
+    // let token = req.body.token;
 
-    async function verify() {
-        const ticket = await client.verifyIdToken({
-            idToken: token,
-            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-        });
-        const payload = ticket.getPayload();
-        const userid = payload['sub'];
-      }
-      verify()
-      .then(()=>{
-          res.cookie('session-token', token);
-          res.send('success')
-      })
-      .catch(console.error);
+    // async function verify() {
+    //     const ticket = await client.verifyIdToken({
+    //         idToken: token,
+    //         audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+    //     });
+    //     const payload = ticket.getPayload();
+    //     const userid = payload['sub'];
+    //   }
+    //   verify()
+    //   .then(()=>{
+    //       res.cookie('session-token', token);
+    //       res.send('success')
+    //   })
+    //   .catch(console.error);
 
 
-    const googleAccessToken = req.body.googleAccessToken;
-    console.log(req.body)
+    // const googleAccessToken = req.body.googleAccessToken;
+    // console.log(req.body)
     // axios
     //     .get("https://www.googleapis.com/oauth2/v3/userinfo", {
     //     headers: {
