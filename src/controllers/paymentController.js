ab4fe3ad16e95d99d2141fd70f4b560e571ee697
asafwithc <asafemin06@gmail.com>
2023-09-04 10:00:55 +0300

    payment services

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,7 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
 const Booking = require('../models/booking');
 
-const endpointSecret = 'whsec_a3ed6bb8e982c413c9713227d86bdfff85a7c5d7100b6bf55cc56da359aae5a9';
+// const endpointSecret = 'whsec_a3ed6bb8e982c413c9713227d86bdfff85a7c5d7100b6bf55cc56da359aae5a9';
 
 
 exports.paymentSheet = async (req, res) => {
@@ -17,36 +17,36 @@ exports.paymentSheet = async (req, res) => {
     });
 }
 
-exports.managePostPayment = (req, res) => {
-    let event = request.body;
+// exports.managePostPayment = (req, res) => {
+//     let event = request.body;
 
-    if (endpointSecret) {
-        const signature = request.headers['stripe-signature'];
-        try {
-          event = stripe.webhooks.constructEvent(
-            request.body,
-            signature,
-            endpointSecret
-          );
-        } catch (err) {
-          console.log(`⚠️  Webhook signature verification failed.`, err.message);
-          return response.sendStatus(400);
-        }
-    }
+//     if (endpointSecret) {
+//         const signature = request.headers['stripe-signature'];
+//         try {
+//           event = stripe.webhooks.constructEvent(
+//             request.body,
+//             signature,
+//             endpointSecret
+//           );
+//         } catch (err) {
+//           console.log(`⚠️  Webhook signature verification failed.`, err.message);
+//           return response.sendStatus(400);
+//         }
+//     }
 
-    switch (event.type) {
-        case 'payment_intent.succeeded':
-          const paymentIntentSucceeded = event.data.object;
-          // Then define and call a function to handle the event payment_intent.succeeded
-          break;
-        // ... handle other event types
-        default:
-          console.log(`Unhandled event type ${event.type}`);
-    }
+//     switch (event.type) {
+//         case 'payment_intent.succeeded':
+//           const paymentIntentSucceeded = event.data.object;
+//           // Then define and call a function to handle the event payment_intent.succeeded
+//           break;
+//         // ... handle other event types
+//         default:
+//           console.log(`Unhandled event type ${event.type}`);
+//     }
     
     
-    console.log(req.body.type);
+//     console.log(req.body.type);
 
 
-    res.status(200).json({message: req.body.type});
-}
+//     res.status(200).json({message: req.body.type});
+// }
--- src/routes/payment.js
+++ src/routes/payment.js
@@ -5,6 +5,6 @@ const paymentController = require('../controllers/paymentController');
 
 router.get('/payment', paymentController.paymentSheet);
 
-router.get('/post-payment', paymentController.managePostPayment);
+// router.get('/post-payment', paymentController.managePostPayment);
 
 module.exports = router;
